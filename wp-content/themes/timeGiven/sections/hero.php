90736e64a4be3686894e5f6b303f0806553c9302
dimaMixd <48953247+dimaMixd@users.noreply.github.com>
2020-05-31 22:45:11 +0300

    Update hero.php

--- wp-content/themes/timeGiven/sections/hero.php
+++ wp-content/themes/timeGiven/sections/hero.php
@@ -1,10 +1,10 @@
-<!-- <div class="loader" id="loader">
+<div class="loader" id="loader">
     <img src="<?php echo get_template_directory_uri().'/images/logo-white.svg' ?>" width="150" class="logo-white" alt="">
     <div class="bar bar-1"></div>
     <div class="bar bar-2"></div>
     <div class="bar bar-3"></div>
     <div class="bar bar-4"></div>
-</div> -->
+</div>
 <section class="section section--hero" id="hero" style="background-image: url(<?php echo get_field('background_image'); ?>);">
     <div class="container">
         <?php include 'nav.php'; ?>
